c10fa64f1d4d68efefbd03ba055476470906a205
user <gv.calita@gmail.com>
2020-11-29 18:33:45 +0100

    /books, /authors

--- src/main/java/guru/springframework/spring5webapp/domain/Book.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Book.java
@@ -16,6 +16,9 @@ public class Book {
 
     private String title;
     private String isbn;
+    
+    @OneToOne
+    private Publisher publisher;
 
     @ManyToMany
     @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
@@ -25,9 +28,22 @@ public class Book {
     public Book() {
     }
 
+    public Book(String title, String isbn, Set<Author> authors) {
+	this.title = title;
+	this.isbn = isbn;
+	this.authors = authors;
+    }
+
     public Book(String title, String isbn) {
         this.title = title;
         this.isbn = isbn;
+    }
+
+    public Book(String title, String isbn, Publisher publisher, Set<Author> authors) {
+	this.title = title;
+	this.isbn = isbn;
+	this.publisher = publisher;
+	this.authors = authors;
     }
 
     public Long getId() {
@@ -60,6 +76,14 @@ public class Book {
 
     public void setAuthors(Set<Author> authors) {
         this.authors = authors;
+    }
+
+    public Publisher getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(Publisher publisher) {
+        this.publisher = publisher;
     }
 
     @Override
